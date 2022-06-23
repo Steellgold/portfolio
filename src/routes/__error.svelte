695d9218588d608ee7da9ae428fddbb1592664f4
Gaëtan H <steellgold0@gmail.com>
2022-06-24 00:23:56 +0200

    Total discovery (Thanks @Bluzzi)

---
+++ src/routes/__error.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+    
+    import type { LoadEvent } from "@sveltejs/kit";
+    
+    export function load({ error, status }: LoadEvent) {
+      return {
+        props: {
+          errorCode: status,
+          message: error?.message,
+        }
+      };
+    }
+
+</script>
+  
+<script lang="ts">
+    export let errorCode: number|null;
+    export let message: string|null;
+</script>
+
+<title>{errorCode}</title>
+<h1>{message}</h1>
